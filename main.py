62ec7010932bc351192fb40cd0d9460c3c4c6f64
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2021-09-18 14:00:15 -0400

    Started visualization for array

--- main.py
+++ main.py
@@ -5,19 +5,27 @@ import curses
 from debugger import debug
 
 
-def sample(a, b):
-    # arr = []
-    # for i in range(b):
-    #     arr.append(a)
-    # return arr
-    q = deque()
+# def sample(a, b):
+#     # arr = []
+#     # for i in range(b):
+#     #     arr.append(a)
+#     # return arr
+#     q = deque()
+#     for i in range(b):
+#         q.append(a)
+#     return q
+
+
+def sample1(a, b):
+    arr = []
     for i in range(b):
-        q.append(a)
-    return q
+        arr.append(a)
+    return arr
 
 
 if __name__ == "__main__":
     os.system('color')
     curses.start_color()
     curses.echo()
-    debug(sample, (2, 5))
+    # debug(sample, (2, 5))
+    debug(sample1, (3, 5))
--- vector.py
+++ vector.py
@@ -1,6 +1,25 @@
 class Vector:
-    def __init__(self, arr):
+    def __init__(self, screen, arr):
         self.val = arr
-    
+        self.screen = screen
+
     def visualize(self):
-        print(self.val, "CALLED")
+        # print(self.val, "CALLED")
+        # y, x = self.screen.getmaxyx()
+        i = 0
+        for x in self.val:
+            print(x)
+            self.screen.border(0)
+            box1 = self.screen.subwin(5 + i*5, 5 + i*5, 5, 5)
+            self.screen.addstr(5 + i*5, 5 + i*5, str(x))
+            box1.box()
+            self.screen.getch()
+            i += 1
+
+        self.screen.refresh()
+# for elem in self.queue:
+#             box1 = self.screen.subwin(4, 20, y_pos, int(x/2))
+#             self.screen.addstr(y_pos + 2, int(x/2) + 10, str(elem))
+#             box1.border(0)
+#             box1.box()
+#             y_pos += 4
